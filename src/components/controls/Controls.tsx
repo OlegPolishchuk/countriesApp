bfa13c473cd2950184ce59a41578069bda89cd97
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-10-20 13:22:53 +0300

    feat: add Custom select component styles

--- src/components/controls/Controls.tsx
+++ src/components/controls/Controls.tsx
@@ -1,24 +1,56 @@
 import React, { useState } from 'react';
 
+import styled from 'styled-components';
+
 import { CustomSelect } from 'components/customSelect/CustomSelect';
 import { Search } from 'components/search/Search';
 import { ReturnComponentType } from 'types';
 
-const options = [
-    { value: 'Africa', label: 'Africa' },
-    { value: 'America', label: 'America' },
-    { value: 'Asia', label: 'Asia' },
-    { value: 'Europe', label: 'Europe' },
-    { value: 'Oceania', label: 'Oceania' },
+interface IOption {
+  value: string;
+  label: string;
+}
+const options: IOption[] = [
+  { value: 'Africa', label: 'Africa' },
+  { value: 'America', label: 'America' },
+  { value: 'Asia', label: 'Asia' },
+  { value: 'Europe', label: 'Europe' },
+  { value: 'Oceania', label: 'Oceania' },
 ];
 
-export const Controls = (): ReturnComponentType => {
-    const [search, setSearch] = useState('');
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 
-    return (
-        <div>
-            <Search search={search} setSearch={setSearch} />
-            <CustomSelect options={options} />
-        </div>
-    );
+  @media (min-width: 767px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+export const Controls = (): ReturnComponentType => {
+  const [search, setSearch] = useState('');
+  const [region, setRegion] = useState<IOption | null>(null);
+
+  const handleChangeOption = (option: unknown): void => {
+    const value = option as IOption;
+
+    setRegion(value);
+  };
+
+  return (
+    <Wrapper>
+      <Search search={search} setSearch={setSearch} />
+      <CustomSelect
+        options={options}
+        placeholder="Filter by Region"
+        isClearable
+        isSearchable={false}
+        value={region}
+        onChange={handleChangeOption}
+      />
+    </Wrapper>
+  );
 };
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -6,42 +6,43 @@ import styled from 'styled-components';
 import { ReturnComponentType } from 'types';
 
 interface Props {
-    search: string;
-    setSearch: (value: string) => void;
+  search: string;
+  setSearch: (value: string) => void;
 }
 
 const InputContainer = styled.label`
-    background-color: var(--colors-ui-base);
-    padding: 1rem 2rem;
-    display: flex;
-    align-items: center;
+  background-color: var(--colors-ui-base);
+  padding: 1rem 2rem;
+  display: flex;
+  align-items: center;
 
-    border-radius: var(--radii);
-    box-shadow: var(--shadow);
-    width: 100%;
-    margin-bottom: 1.5rem;
+  border-radius: var(--radii);
+  box-shadow: var(--shadow);
+  width: 100%;
+  margin-bottom: 1.5rem;
 
-    @media (min-width: 767px) {
-        margin-bottom: 0;
-        width: 280px;
-    }
+  @media (min-width: 767px) {
+    margin-bottom: 0;
+    width: 280px;
+  }
 `;
 
 const Input = styled.input.attrs({
-    type: 'search',
-    placeholder: 'Search for a country...',
+  type: 'search',
+  placeholder: 'Search for a country...',
 })`
-    margin-left: 2rem;
-    border: none;
-    outline: none;
-    color: var(--colors-text);
+  margin-left: 2rem;
+  border: none;
+  outline: none;
+  background-color: var(--colors-ui-base);
+  color: var(--colors-text);
 `;
 
 export const Search = ({ setSearch, search }: Props): ReturnComponentType => {
-    return (
-        <InputContainer>
-            <IoSearch />
-            <Input onChange={e => setSearch(e.currentTarget.value)} value={search} />
-        </InputContainer>
-    );
+  return (
+    <InputContainer>
+      <IoSearch />
+      <Input onChange={e => setSearch(e.currentTarget.value)} value={search} />
+    </InputContainer>
+  );
 };
